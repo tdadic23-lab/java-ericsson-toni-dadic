a2b05d64f1ba0887941ee53048cb06a98c9626d8
Toni Dadic <toni.dadic@ericsson.com>
2025-12-02 11:09:39 +0100

    Armstrong numbers with changable limit

--- ENT_Java_tecaj/src/Main.java
+++ ENT_Java_tecaj/src/Main.java
@@ -1,23 +1,33 @@
 public class Main {
     public static void main(String[] args){
-        printArmstrongNumbers();
-    }
 
-    public static void printArmstrongNumbers(){
-        for(int i = 1; i < 10000; i++){
-            int sum = 0;
-            int temp = i;
+        int limit = 20000;
 
-            while (temp > 0){
-                int digit = temp % 10;
-                sum += digit * digit * digit;
-                temp /= 10;
-            }
+        if (limit < 1){
+            System.out.println("Limit mora biti veći od nule.");
+        }
+        else{
+            System.out.println("Armstrongovi brojevi između 1 i " + limit + " su: ");
 
-            if(sum == i){
-                System.out.println(i);
+            for (int i = 1; i <= limit; i++){
+                if (isArmstrong(i)){
+                    System.out.println(i);
+                }
             }
         }
+    }
+
+    public static boolean isArmstrong(int number){
+        int sum = 0;
+        int temp = number;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        return sum == number;
     }
 }
 
