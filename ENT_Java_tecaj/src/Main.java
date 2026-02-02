087a62eba1208b47fabfab1fabf39216492d9632
Toni Dadic <toni.dadic@ericsson.com>
2026-02-02 09:40:20 +0100

    Zadatak 2.1, prepravak

--- ENT_Java_tecaj/src/Main.java
+++ ENT_Java_tecaj/src/Main.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        characterCounter();
+        Scanner scanner = new Scanner(System.in);
+        characterCounter(scanner);
+        scanner.close();
     }
 
-    public static void characterCounter(){
+    public static void characterCounter(Scanner scanner){
 
         System.out.print("Unesi string: ");
-        Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
 
         int letters = 0;
@@ -30,7 +31,6 @@ public class Main {
         System.out.printf("Slova: %d%n", letters);
         System.out.printf("Brojevi: %d%n", digits);
         System.out.printf("Ostali znakovi: %d%n", others);
-
     }
 }
 
